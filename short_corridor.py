aed6a2afd6222c65bf5bcad0c2f583f2d81140c8
Ziyang Zhang <34000956+antaressgzz@users.noreply.github.com>
2018-05-06 18:49:09 +0100

    modification

--- short_corridor.py
+++ short_corridor.py
@@ -1,43 +1,43 @@
 """
+This is an relization of example 13.1 on 'Rainforcement Learning: An Introduction'
+by Sutton and Barto(v2018), a 3 grid corridor. In state 0 and 1, action 0 leads
+to left(if in state 0 then no change happens) and action 1 leads to right, 
+while in state 2, the result of the two actions are reversed. All 3 states are 
+indistinguishable to the agent(same observation). The optimal result is stocastic,
+probability of 0.59 turn right(action 1) and reward is about 11(on average).
+
 @author: Ziyang Zhang
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-class short_corridor:
-    def __init__(self):
-        self.state = None
-        
+class short_corridor:       
     def reset(self):
        self.state = 0
        return self.state
     
-    def step(self, action):
-        
+    def step(self, action):      
         if self.state == 0:
-            if action == 1: state = 1
-            else:           state = 0
-                
+            if action == 1: state = 1 # action 1 leads to right
+            else:           state = 0               
         elif self.state == 1:
             if action == 0: state = 2
-            else:           state = 0
-                
+            else:           state = 0 # action 1 leads to left               
         elif self.state == 2:
             if action == 1: return None, 0, True
-            else:           state = 1
-                
-        self.state = state
-                
+            else:           state = 1               
+        self.state = state                
         return self.state, -1, False
         
 class Agent:
     
     def __init__(self):
         self.alpha = 2 ** -12
-        self.theta = np.array([0.0,0.0])
+        self.theta = np.zeros(2)
         self.feature = np.array([[0.0, 1.0],
                                  [1.0, 0.0]])
+        self.probs = []
         self.clear_memory()
         
     def clear_memory(self):
@@ -62,25 +62,25 @@ class Agent:
         G = np.sum(self.rewards)
         episode = len(self.rewards)
         for t in range(episode):
-#            observ = self.observs[t]
             action = self.actions[t]
             h = np.dot(self.theta, self.feature)
             probs = np.exp(h) / np.sum(np.exp(h))
-            print(probs)
+            self.probs.append(probs[1]) # record the policy(prob to choose action 1)
             dlnp = self.feature[:,action] - np.dot(probs, self.feature.T)
             self.theta += self.alpha * G * dlnp
             G -= self.rewards[t]
         self.clear_memory()
-        
+
 
 env = short_corridor()
 agent = Agent()
 rewards_trend = []
+moving_average = []
 
-for episode in range(1000):
+for episode in range(3000):
     observ = env.reset()
     episode_rewards = 0
-    while episode_rewards > -1000:
+    while True:
         action = agent.choose_action(observ)
         observ_, reward, done = env.step(action)
         agent.store(observ, action, reward)
@@ -89,13 +89,17 @@ for episode in range(1000):
             break
         observ = observ_
     print('episode:', episode, 'reward:', episode_rewards)
-    try:
-        rewards_trend.append(0.99*rewards_trend[episode-1]+0.01*episode_rewards)
-    except:
-        rewards_trend.append(episode_rewards)       
+    rewards_trend.append(episode_rewards)
+    if episode > 100:
+        moving_average.append(np.sum(rewards_trend[-100:]) / 100)
     agent.learn()
     
-plt.plot(np.arange(len(rewards_trend)), rewards_trend)
+plt.plot(np.arange(len(moving_average)), moving_average)
 plt.ylabel('Rewards')
+plt.xlabel('episode')
+plt.show()
+
+plt.plot(np.arange(len(agent.probs)), agent.probs)
+plt.ylabel('probs to right')
 plt.xlabel('episode')
 plt.show()
